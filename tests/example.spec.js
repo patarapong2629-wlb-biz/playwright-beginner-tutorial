85b69648bb7dfc3c0975beafde2c100341e5a868
patarapong2629-wlb-biz <patarapong@welovebug.biz>
2025-07-17 08:50:01 +0700

    Add trace in a specific test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,18 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title', async ({ page, context }) => {
+  await context.tracing.start({
+    snapshots: true,
+    screenshots: true,
+  });
+
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+
+  await context.tracing.stop({ path: 'test1_trace.zip' });
 });
 
 test('get started link', async ({ page }) => {
@@ -15,5 +22,7 @@ test('get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole('heading', { name: 'Installation' })
+  ).toBeVisible();
 });
