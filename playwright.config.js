b8661c599078ec1b881f97bf6d8a8f7506ab06fb
patarapong2629-wlb-biz <patarapong@welovebug.biz>
2025-07-20 12:33:19 +0700

    Allow test.only

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,8 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
+  forbidOnly: false,
   /* Retry on CI only */
   // retries: process.env.CI ? 2 : 0,
   retries: 1,
